77764d21b65695651151fb466bbd88fe6acdcd1c
tehnools <noelsoong@live.com>
2017-03-12 16:20:29 +1300

    added better exceptions using if statements instead of try

--- R1.py
+++
@@ -1,7 +0,0 @@
-import re
-
-a = "test"
-b = "test"
-
-if re.match(r"test", a):
-    print("match")
--- scanner.py
+++ scanner.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 class Scanner():
 
@@ -8,23 +9,22 @@ class Scanner():
         @Return None"""
         self.filename = filename
         self.current_token_index = 0
-
-    # def scanline(self, line):
-    #     for token in line[0].split(" "):
-    #          return(token)
+        self.line_index = 0
 
     def scan(self):
-        """Recieves file path in the form of a strings then executes interpret_line
+        """Recieves file path in the form of a strings
+        then executes interpret_line
         @var f,
         @funct isinstance, readlines, interpret_line
         @Return None"""
-        # returns an array with full lines of strings
         if isinstance(self.filename, basestring):
             f = open(self.filename, "rw")
             for line in f.readlines():
+                self.line_index += 1
                 self.interpret_line(line.splitlines())
         else:
-            raise("FILE ERROR: FILENAME MUST BE A STRING")
+            print("FILENAME MUST BE A STRING")
+            sys.exit()
 
     def interpret_line(self, full_line):
         """ Recieves a line at a time splits the lines by space and calls switch
@@ -45,17 +45,20 @@ class Scanner():
             self.new_router(line)
         elif re.match("#"+".*?", token):
             self.current_token_index = len(line)
-        # else:
-        #     print(line[self.current_token_index]+ " Does not Match")
+        else:
+            print("LINE " + str(self.line_index) +
+            " : Does not follow proper Syntax at... " + line[0])
+            sys.exit()
 
     def new_router(self, line):
-        """Creates New router checks if next token is an integer"""
-        print("new_router")
+        """Creates New instance of router checks if next token is an integer
+        @Return None"""
         if re.match(r"\d", line[self.current_token_index + 1]):
             self.current_token_index += 1
-            print("router id match")
         else:
-            print("ERROR INVALID EXECUTION: integer must follow router-id")
+            print("LINE " + str(self.line_index) +
+            " : Integer must follow router-id")
+            sys.exit()
 
 def main():
     """Main executions here"""
